31d8ae2285b939367f27f57faac50d8781dbb586
fr33wayz <fr33wayz@gmail.com>
2016-04-04 07:27:08 +0100

    initial project setup

---
+++ kode_kloud/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kode_kloud project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kode_kloud.settings")
+
+application = get_wsgi_application()
